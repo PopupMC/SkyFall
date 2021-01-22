62c9ea9aa0c1f6503edd064d9992d454eabec8ba
June Hanabi <junehanabi@gmail.com>
2021-01-21 19:48:05 -0500

    Made the teleportation a little laggy and other otpimizations to help the server

--- src/main/java/com/popupmc/skyfall/OnPlayerMoveEvent.java
+++ src/main/java/com/popupmc/skyfall/OnPlayerMoveEvent.java
@@ -1,5 +1,6 @@
 package com.popupmc.skyfall;
 
+import org.bukkit.Location;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -8,9 +9,23 @@ public class OnPlayerMoveEvent implements Listener {
     @EventHandler
     public void onPlayerMoveEvent(PlayerMoveEvent e) {
 
-        // Attempt a teleport on every move
-        // This sounds really inefficient but it's actually not
-        // If I optimized it any with extra checks it would be exactly the same efficiency
+        // Lag a bit to help the server out
+        skipCounter++;
+        if((skipCounter % 5) != 0)
+            return;
+
+        // Do a hasty rough check
+        // Basically nobody needs teleporting anywhere if not within these coordinates
+        // Another little optimization to help the server out
+        Location loc = e.getPlayer().getLocation();
+
+        if(loc.getBlockY() < DoTeleport.netherCeiling &&
+            loc.getBlockY() > 0)
+            return;
+
+        // Now try to teleport the person
         DoTeleport.doTeleport(e.getPlayer());
     }
+
+    public static int skipCounter = 0;
 }
